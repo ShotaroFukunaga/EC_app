439dab67ca8d4061259fd473023e8f6db837a564
ShotaroFukunaga <accelerator.engin@gmail.com>
2022-05-25 13:34:10 +0900

    component_props

--- resources/views/components/tests/card.blade.php
+++ resources/views/components/tests/card.blade.php
@@ -1,4 +1,10 @@
-<!-- 属性（props）を渡すパターン -->
+<!-- 初期値の設定 ※propsで設定する場合、それぞれの変数に初期値を設定しなくてはならない-->
+@props([
+        'title',
+        'message' => '初期値です。',
+        'content' => 'コンテンツ初期値です'
+        ])
+<!-- 属性を渡すパターン -->
 <div class="border-2 shadow-md w-1/4 p-2">
   <div>{{ $title }}</div>
   <div>画像</div>
--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -5,4 +5,5 @@
   <!-- 属性（props）パターンコンポーネントに属性を着けて、下記のように属性に対して値を渡すことができる -->
   <x-tests.card title="タイトル1" content="コンテンツ1" :message="$message"/>
   <!-- 変数（:meesage） :←ダブルコロンをつけることで変数だと明示的に表現する -->
+  <x-tests.card title="タイトル２"></x-tests.card>
 </x-tests.app>
