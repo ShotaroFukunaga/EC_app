c22d41f404a939ac7c003396010ee94f93771292
ShotaroFukunaga <accelerator.engin@gmail.com>
2022-05-26 11:51:23 +0900

    サービスコンテナ

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ComponentTestController;
-
+use App\Http\Controllers\LifeCycleTestController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -25,5 +25,6 @@ Route::get('/dashboard', function () {
 Route::get('/component-test1', [ComponentTestController::class, 'showComponent1']);
 Route::get('/component-test2', [ComponentTestController::class, 'showComponent2']);
 
+Route::get('/servicecontainertest', [LifeCycleTestController::class, 'showServiceContainerTest']);
 //現在のディレクトリのauth.phpというファイルを呼び込む
 require __DIR__.'/auth.php';
