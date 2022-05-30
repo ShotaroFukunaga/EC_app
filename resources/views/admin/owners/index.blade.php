b9c9b09d4720664d82d80b0da411468da4d90377
ShotaroFukunaga <accelerator.engin@gmail.com>
2022-05-30 13:18:51 +0900

    ownerStore

--- resources/views/admin/owners/index.blade.php
+++ resources/views/admin/owners/index.blade.php
@@ -25,11 +25,10 @@
                     
                     
                       
-                      
-                      
-                    
                     <section class="text-gray-600 body-font">
                       <div class="container px-5  mx-auto">
+                        <x-flash-message status="info"/>
+                        {{-- フラッシュメッセージにステータスを渡して背景色を変更する --}}
                         <div class="flex justify-end mb-4">
                           <button onclick="location.href='{{ route('admin.owners.create') }}'"class="text-white bg-yellow-500 border-0 py-2 px-8 focus:outline-none hover:bg-yellow-600 rounded text-lg">新規登録</button>
                         </div>
